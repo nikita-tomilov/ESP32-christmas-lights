8d9840289a1bbc17f9375e34b1c81d3e94fd5bf5
Nikita Tomilov <programmer174@icloud.com>
2021-11-21 19:24:01 +0300

    leds.hpp: migrate to FastLED; adafruit lib is glitchy on ESP32

--- leds.hpp
+++ leds.hpp
@@ -1,26 +1,23 @@
 #ifndef _LEDS_HPP
 #define _LEDS_HPP
 
-//make sure to use 1.7 or higher on ESP32
-//see https://github.com/adafruit/Adafruit_NeoPixel/issues/139
-#include <Adafruit_NeoPixel.h>
+#include <FastLED.h>
 
 #define NUMPIXELS 50
-
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, LED_STRIP_PIN, NEO_GRB + NEO_KHZ800);
+CRGB leds[NUMPIXELS];
 
 void setPixelColor(int i, int r, int g, int b) {
-  pixels.setPixelColor(i, pixels.Color(r, g, b));
-}
-
-void initLeds() {
-  pixels.begin();
-  setPixelColor(0, 255, 0, 0);
-  pixels.show();
+  leds[i] = CRGB(r, g, b);
 }
 
 void drawLeds() {
-  pixels.show();
+  FastLED.show();
+}
+
+void initLeds() {
+  FastLED.addLeds<NEOPIXEL, LED_STRIP_PIN>(leds, NUMPIXELS);
+  setPixelColor(0, 255, 0, 0);
+  drawLeds();
 }
 
 #endif
